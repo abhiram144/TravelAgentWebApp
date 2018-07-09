d18827b611da3d13cf8b27f68e99b69e56d3c24d
abhiram144 <venkata.abhiram144@gmail.com>
2018-07-09 20:17:11 +0530

    EchoBot Fix

--- Travel Website/Controllers/MessagesController.cs
+++ Travel Website/Controllers/MessagesController.cs
@@ -5,7 +5,7 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Web.Http.Description;
 using System.Net.Http;
-using Microsoft.Bot.Sample.SimpleEchoBot;
+using Travel_Website.SimpleEchoBot;
 
 namespace Travel_Website.Controllers
 {
