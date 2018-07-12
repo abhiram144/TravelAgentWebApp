9c8f198e583eb93f71b194e5d84bc732032cb092
abhiram144 <venkata.abhiram144@gmail.com>
2018-07-12 12:04:22 +0530

    Revert "Added Travel Agent"
    This reverts commit a636fa72babada55102047e4f28b417f73f7cc70.

--- Travel Agent Bot/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Travel_Website.MvcApplication" Language="C#" %>
--- Travel Agent Bot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=travelagent144-Web-Deploy.pubxml -p:Password=9GhZWHA8JiPLKRrdGrDd5H08hQ69RuLDQJDPhbh1gjiwB4EgyzBjZzvPEMlZ
-
