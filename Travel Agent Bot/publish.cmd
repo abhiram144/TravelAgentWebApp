a636fa72babada55102047e4f28b417f73f7cc70
abhiram144 <venkata.abhiram144@gmail.com>
2018-07-12 11:58:20 +0530

    Added Travel Agent

---
+++ Travel Agent Bot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=travelagent144-Web-Deploy.pubxml -p:Password=9GhZWHA8JiPLKRrdGrDd5H08hQ69RuLDQJDPhbh1gjiwB4EgyzBjZzvPEMlZ
+
